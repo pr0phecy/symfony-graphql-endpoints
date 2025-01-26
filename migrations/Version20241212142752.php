a358406ea2d8f352d75a6e7dc55d7e6a498e5c53
jelle.monen <jelle.monen@true.nl>
2025-01-26 19:37:47 +0100

    chore: add uuids to migrations

--- migrations/Version20241212142752.php
+++ migrations/Version20241212142752.php
@@ -7,7 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-
 final class Version20241212142752 extends AbstractMigration
 {
     public function getDescription(): string
@@ -17,10 +16,12 @@ final class Version20241212142752 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE trade (id INT NOT NULL, number VARCHAR(10) NOT NULL, date DATE NOT NULL, note VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+        $this->addSql('CREATE TABLE trade (id UUID NOT NULL DEFAULT uuid_generate_v4(), number VARCHAR(10) NOT NULL, date DATE NOT NULL, note VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_7E1A436696901F54 ON trade (number)');
 
-        $this->addSql('CREATE TABLE transaction (id INT NOT NULL, trade_id INT NOT NULL, client_name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, commodity VARCHAR(255) NOT NULL, volume INT NOT NULL, type VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE transaction (id UUID NOT NULL DEFAULT uuid_generate_v4(), trade_id UUID NOT NULL, client_name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, commodity VARCHAR(255) NOT NULL, volume INT NOT NULL, type VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_723705D1C2D9760 ON transaction (trade_id)');
 
         $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1C2D9760 FOREIGN KEY (trade_id) REFERENCES trade (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
